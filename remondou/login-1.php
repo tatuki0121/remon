53486a848ec57bc0b72664edfa220d195328237c
keishu0907 <2201395@s.asojuku.ac.jp>
2023-11-07 11:15:52 +0900

    login

--- remondou/login-1.php
+++ remondou/login-1.php
@@ -1,3 +1,4 @@
+<?php session_start(); ?>
 <?php require 'db-connect.php'; ?>
 <?php require 'header.php'; ?>
 <?php require 'nav.php'; ?>
@@ -6,11 +7,27 @@
 <form action="top.php" method="post">
     メールアドレス　<input type="text" name="mail"><br>
     パスワード　<input type="password" name="pass"><br>
-    <input type="submit" value="ログイン">
+    <p><input type="submit" value="ログイン"></p>
     <?php
+    unset($_SESSION['user']);
+    $pdo=new PDO($connect, USER, PASS);
+    
+    //loginIDで検索する
+    $sql=$pdo->prepare('select * from user where login=?');
+    $sql->execute([$_POST['login']]);
+    
+    // echo password_hash( $_POST['password'], PASSWORD_DEFAULT);
+    foreach($sql as $row){
+        // if(password_verify($_POST['password'], $row['password']) == true){
+        if(password_verify($_POST['password'], $row['password']) == true){
+            //ログイン済みのユーザー情報をセッションに格納
+            $_SESSION['customer']=['id'=>$row['id'], 'name'=>$row['name'], 'address'=>$row['address'], 'login'=>$row['login'], 'password'=>$row['password']];
+        }
+    }
     if(!isset($_SESSION['user'])){
     echo 'メールアドレスまたはパスワードが違います。';
-    }else if (empty($mail) || empty($pass)) {
+    }
+    if (empty($mail) || empty($pass)) {
         echo "メールアドレスとパスワードは必須項目です。";
     }
     ?>
