0b6f6a183e1aef36dbaae33bef917ce77b48f4af
hakutojp <2201365@s.asojuku.ac.jp>
2023-11-07 11:18:01 +0900

    新規ユーザー登録

--- remondou/sign-up-1.php
+++ remondou/sign-up-1.php
@@ -1,8 +1,14 @@
+<?php session_start(); ?>
 <?php require 'db-connect.php'; ?>
 <?php require 'header.php'; ?>
 <?php
+    $pdo = new PDO($connect,USER,PASS);
     echo '新規ユーザー登録';
+    echo '<form action="sign-up-1.php" method="post">';
     echo '<p>メールアドレス<input type="text" name="mail"></p>';
     echo '<p>パスワード<input type="text" name="pass"></p>';
+    echo '<input type="button" value="登録">';
+    echo '</form>';
+    echo '';
 ?>
 <?php require 'footer.php'; ?>
