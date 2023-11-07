682c5087529c9cf61f3d0b69e9f614504e1fcfde
hakutojp <2201365@s.asojuku.ac.jp>
2023-11-07 10:17:23 +0900

    新規登録画面

---
+++ remondou/sign-up-1.php
@@ -0,0 +1,8 @@
+<?php require 'db-connect.php'; ?>
+<?php require 'header.php'; ?>
+<?php
+    echo '新規ユーザー登録';
+    echo '<p>メールアドレス<input type="text" name="mail"></p>';
+    echo '<p>パスワード<input type="text" name="pass"></p>';
+?>
+<?php require 'footer.php'; ?>
