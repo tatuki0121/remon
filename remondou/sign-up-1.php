a5d2ae3f8335d530afe616fba872b5da27188e5e
hakutojp <2201365@s.asojuku.ac.jp>
2023-11-07 13:14:52 +0900

    新規ユーザー登録

--- remondou/sign-up-1.php
+++ remondou/sign-up-1.php
@@ -2,13 +2,21 @@
 <?php require 'db-connect.php'; ?>
 <?php require 'header.php'; ?>
 <?php
+if(isset($_POST['send'])){
+    // DB問合せ
     $pdo = new PDO($connect,USER,PASS);
+
+    if(){
+        
+    }
+    // 存在してる場合→エラーメッセージ　画面はこのまま
+    // 存在しない場合、セッションに登録して確認画面へ遷移 header(location)
+}
     echo '新規ユーザー登録';
     echo '<form action="sign-up-1.php" method="post">';
     echo '<p>メールアドレス<input type="text" name="mail"></p>';
     echo '<p>パスワード<input type="text" name="pass"></p>';
-    echo '<input type="button" value="登録">';
+    echo '<input type="button" value="登録" name="send">';
     echo '</form>';
-    echo '';
 ?>
 <?php require 'footer.php'; ?>
