9a32e8c702ebe421795feb607481b9d1c371951e
keishu0907 <2201395@s.asojuku.ac.jp>
2023-11-08 10:53:46 +0900

    login完成

--- remondou/login-1.php
+++ remondou/login-1.php
@@ -2,37 +2,43 @@
 <?php require 'db-connect.php'; ?>
 <?php require 'header.php'; ?>
 <?php require 'nav.php'; ?>
-<h1>ログイン</h1>
-<p>会員の方は登録時に入力されたE-mailとパスワードでログインしてください。</p>
-<form action="top.php" method="post">
+
+    <?php
+        if(isset($_POST['but'])){
+            unset($_SESSION['user']);
+            if (empty($_POST['mail']) || empty($_POST['pass'])) {
+                echo "メールアドレスとパスワードは必須項目です。";
+            }else{
+                $pdo=new PDO($connect, USER, PASS);
+                
+                //メールアドレスで検索する
+                $sql=$pdo->prepare('select * from user where mail=?');
+                $sql->execute([$_POST['mail']]);
+                
+                foreach($sql as $row){
+                    if(password_verify($_POST['pass'], $row['pass']) == true){
+                        //ログイン済みのユーザー情報をセッションに格納
+                        $_SESSION['user']=['user_id'=>$row['user_id'], 'mail'=>$row['mail'], 'pass'=>$row['pass'], 'torokubi'=>$row['torokubi'], 'kousinbi'=>$row['kousinbi']];
+                    }
+                }
+                if(isset($_SESSION['user'])){
+                    header("Location: top.php");
+                    exit();
+                }else{
+                    echo 'メールアドレスまたはパスワードが違います。';
+                }
+            }
+        }
+    ?>
+    <h1>ログイン</h1>
+    <p>会員の方は登録時に入力されたE-mailとパスワードでログインしてください。</p>
+    <form action="login-1.php" method="post">
     メールアドレス　<input type="text" name="mail"><br>
     パスワード　<input type="password" name="pass"><br>
-    <p><input type="submit" value="ログイン"></p>
-    <?php
-    unset($_SESSION['user']);
-    $pdo=new PDO($connect, USER, PASS);
-    
-    //loginIDで検索する
-    $sql=$pdo->prepare('select * from user where login=?');
-    $sql->execute([$_POST['login']]);
-    
-    // echo password_hash( $_POST['password'], PASSWORD_DEFAULT);
-    foreach($sql as $row){
-        // if(password_verify($_POST['password'], $row['password']) == true){
-        if(password_verify($_POST['password'], $row['password']) == true){
-            //ログイン済みのユーザー情報をセッションに格納
-            $_SESSION['customer']=['id'=>$row['id'], 'name'=>$row['name'], 'address'=>$row['address'], 'login'=>$row['login'], 'password'=>$row['password']];
-        }
-    }
-    if(!isset($_SESSION['user'])){
-    echo 'メールアドレスまたはパスワードが違います。';
-    }
-    if (empty($mail) || empty($pass)) {
-        echo "メールアドレスとパスワードは必須項目です。";
-    }
-    ?>
-</form>
-<form action="sign-up-1.php" method="post">
-    <input type="submit" value="新規会員登録">
-</form>
+    <p><input type="submit" name="but" value="ログイン"></p>
+    </form>
+    <p>まだ会員登録されていない方</p>
+    <form action="sign-up-1.php" method="post">
+        <input type="submit" value="新規会員登録">
+    </form>
 <?php require 'footer.php'; ?>
