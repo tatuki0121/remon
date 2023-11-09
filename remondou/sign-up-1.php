9a258a297f9eb932698fee374002573cef6f34f7
hakutojp <2201365@s.asojuku.ac.jp>
2023-11-09 10:57:25 +0900

    新規会員登録

--- remondou/sign-up-1.php
+++ remondou/sign-up-1.php
@@ -1,29 +1,27 @@
 <?php session_start(); ?>
 <?php require 'db-connect.php'; ?>
-<?php require 'header.php'; ?>
 <?php
 if(isset($_POST['send'])){
     // DB問合せ
     $pdo = new PDO($connect,USER,PASS);
-    $sql = $pdo->prepare("select * from user where mail=? or pass=? ");
+    $sql = $pdo->prepare("select * from user where mail=? and pass=?");
     $sql->execute([$_POST['mail'],$_POST['pass']]);
     if(empty($sql->fetchAll())){
         // 存在しない場合、セッションに登録して確認画面へ遷移 header(location)
         $_SESSION['user']=[
             'mail' => $_POST['mail'],'pass' => $_POST['pass']
         ];
-        header('Location: https://');
-        exit();
+        header('Location: sign-up-2.php');
     }else{
         // 存在してる場合→エラーメッセージ　画面はこのまま     ]
-        echo 'メールアドレスかパスワードが重複しています。';
+        require 'header.php';
+        echo '新規ユーザー登録';
+        echo '<form action="sign-up-1.php" method="post">';
+        echo '<p>メールアドレス<input type="text" name="mail"></p>';
+        echo '<p>パスワード<input type="password" name="pass"></p>';
+        echo '<input type="submit" value="登録" name="send">';
+        echo '</form>';
     }
 }
-    echo '新規ユーザー登録';
-    echo '<form action="sign-up-1.php" method="post">';
-    echo '<p>メールアドレス<input type="text" name="mail"></p>';
-    echo '<p>パスワード<input type="text" name="pass"></p>';
-    echo '<input type="button" value="登録" name="send">';
-    echo '</form>';
 ?>
 <?php require 'footer.php'; ?>
