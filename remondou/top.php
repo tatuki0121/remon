d1133e45756ff9562c42458300d89fd34446ef91
kashio2201406 <148730004+kashio2201406@users.noreply.github.com>
2023-11-10 13:06:30 +0900

    update css

--- remondou/top.php
+++ remondou/top.php
@@ -24,23 +24,30 @@
 
 <body>
     <div class="slider">
-        <img src="test.img/test1.jpg" width="500" height="300" alt="">
-        <img src="test.img/test2.jpg" width="500" height="300" alt="">
-        <img src="test.img/test3.jpg" width="500" height="300" alt="">
+        <img src="test.img/test1.jpg" width="500" height="400" alt="">
+        <img src="test.img/test2.jpg" width="500" height="400" alt="">
+        <img src="test.img/test3.jpg" width="500" height="400" alt="">
     </div>
-    <div class="heading">
-        <h2>製品情報</h2>
-    </div>
+    <h2 class="item_info">製品情報</h2>
     <ul>
         <li><img src="test.img/test1.jpg" width="300" height="200" alt=""></li>
         <li><img src="test.img/test2.jpg" width="300" height="200" alt=""></li>
         <li><img src="test.img/test3.jpg" width="300" height="200" alt=""></li>
     </ul>
+    <p>
     <h2>こだわり</h2>
-    <p>これが、イマドキのレモンサワー。</p>
-    <p>お家で楽しめる、こだわりのおいしさ。</p>
-    <p><img src="test.img/test1.jpg" width="300" height="200" alt=""></p>
-    <p>～画像説明～</p>
+    </p>
+    <div class="exp">これが、イマドキのレモンサワー。</div>
+    <div class="exp">お家で楽しめる、こだわりのおいしさ。</div>
+    <p><img src="test.img/test1.jpg" width="350" height="250" alt=""></p>
+    <div class="exp">～画像説明～</div>
+    <p>
+    <h2>ムービー</h2>
+    </p>
+    <div style="text-align: center;">
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/1IeAtQiSPMc?si=da3KJ3R9CwDtjT7w" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+    </div>
+    <?php require 'footer.php'; ?>
 </body>
 
 </html>
