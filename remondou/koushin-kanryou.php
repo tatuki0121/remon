07cc094e2fbb2615c3b9d37f1aa8ab73cc658a47
keishu0907 <2201395@s.asojuku.ac.jp>
2023-11-15 10:51:09 +0900

    完了確認修正

--- remondou/koushin-kanryou.php
+++ remondou/koushin-kanryou.php
@@ -1,5 +1,5 @@
 <?php require 'header.php'; ?>
-<?php require 'menu.php'; ?>
+<?php require 'nav.php'; ?>
 <form action="top.php" method="POST">
     <p>更新が完了しました</p>
     <input type="submit" value="トップ画面へ">
