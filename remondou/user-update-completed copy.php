67da57c88889089b1c89e9fdc894b31f2d46494f
hakutojp <2201365@s.asojuku.ac.jp>
2023-11-15 23:47:59 +0900

    ユーザー更新コピー

---
+++ remondou/user-update-completed copy.php
@@ -0,0 +1,7 @@
+<?php require 'header.php'; ?>
+<?php require 'nav.php'; ?>
+<form action="top.php" method="POST">
+    <p>更新が完了しました</p>
+    <input type="submit" value="トップ画面へ">
+</form>
+<?php require 'footer.php'; ?>
