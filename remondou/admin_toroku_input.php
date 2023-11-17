b2ad30c2309b66dd9fdd481555020c7acba3c14e
kashio2201406 <148730004+kashio2201406@users.noreply.github.com>
2023-11-17 13:44:59 +0900

    登録管理画面

--- remondou/admin_toroku_input.php
+++ remondou/admin_toroku_input.php
@@ -1,3 +1,7 @@
+<?php session_start(); ?>
+<?php require 'db-connect.php'; ?>
+<?php require 'admin-header.php'; ?>
+
 <!DOCTYPE html>
 <html lang="ja">
 
@@ -11,38 +15,14 @@
     <h2>商品登録</h2>
     <hr>
 
-    <?php
-    // フォームがサブミットされたかどうかを確認
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        // 入力が空でないかを確認
-        if (empty($_POST['name']) || empty($_POST['capa']) || empty($_POST['dosu']) || empty($_POST['price']) || empty($_POST['suryo']) || empty($_FILES['img']['name']) || empty($_POST['exp'])) {
-            echo "すべての項目を入力してください。";
-        } else {
-            // フォームのデータを `admin_toroku_output.php` に送信
-            $name = $_POST['name'];
-            $capa = $_POST['capa'];
-            $dosu = $_POST['dosu'];
-            $price = $_POST['price'];
-            $suryo = $_POST['suryo'];
-            $img = $_FILES['img']['name'];
-            $exp = $_POST['exp'];
-
-            // データを `admin_toroku_output.php` に送信
-            echo "フォームが正常に送信されました。";
-            header("Location: admin_toroku_output.php?name=$name&capa=$capa&dosu=$dosu&price=$price&suryo=$suryo&img=$img&exp=$exp");
-            exit();
-        }
-    }
-    ?>
-
-    <form action="" method="post" enctype="multipart/form-data">
-        <p>商品名：<input type="text" name="name"></p>
-        <p>内容量：<input type="number" name="capa"></p>
-        <p>度数：<input type="number" name="dosu"></p>
-        <p>価格：<input type="number" name="price"></p>
-        <p>在庫数量：<input type="number" name="suryo"></p>
-        <p>商品画像：<input type="file" name="img"></p>
-        <p>商品説明：<input type="text" name="exp"></p>
+    <form action="admin_toroku_output.php" method="post">
+        <p>商品名：<input type="text" name="name" required></p>
+        <p>内容量：<input type="number" name="capa" required></p>
+        <p>度数：<input type="number" name="dosu" required></p>
+        <p>価格：<input type="number" name="price" required></p>
+        <p>在庫数量：<input type="number" name="suryo" required></p>
+        <p>商品画像：<input type="file" name="img" required></p>
+        <p>商品説明：<input type="text" name="exp" required></p>
         <input type="button" onclick="location.href='adimin-shohinitiran.php'" value="戻る">
         <input type="submit" value="登録">
     </form>
