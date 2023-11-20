879f4ff1044f0fdb587492af2d544dd389114485
2201401 <0113satoshi3104@gmail.com>
2023-11-20 10:55:01 +0900

    Update logout-output-admin.php

--- remondou/logout-output-admin.php
+++ remondou/logout-output-admin.php
@@ -1,13 +1,12 @@
 <?php session_start();?>
-<?php require 'header.php'; ?>
-<?php require 'menu.php'; ?>
-
 <?php
-if(isset($_SESSION['customer'])){
-    unset($_SESSION['customer']);
+if(isset($_SESSION['admin'])){
+    unset($_SESSION['admin']);
     echo 'ログアウト完了しました。';
 }else{
     echo'すでにログアウトしています。';
 }
 ?>
+<form action="admin-login.php">
+    <input type="submit" value="ログイン画面へ">
 <?php require 'footer.php'; ?>
