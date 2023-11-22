ea003c5ee155a81c67c723131f3b13aa4fee46ec
keishu0907 <2201395@s.asojuku.ac.jp>
2023-11-22 11:11:06 +0900

    修正

--- remondou/kounyu.php
+++ remondou/kounyu.php
@@ -1,6 +1,13 @@
+<?php session_start(); ?>
 <?php
 $id = $_GET['id'];
-$_SESSION['item'][$id]=['shohin_id'=>$id,'stock'=>$_POST['stock']];
+if(!isset($_SESSION['item'])){
+    $_SESSION['item']=[];
+}
+$count=0;
+if(isset($_SESSION['item'][$id])){
+    $count=$_SESSION['item'][$id]['stock'];
+}
 if(isset($_POST['nowbuy'])){
     require 'kounyu-kakunin.php';
 }else if(isset($_POST['cart-in'])){
