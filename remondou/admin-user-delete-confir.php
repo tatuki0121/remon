20265dd7a1469581819efbb83e5718805e51c619
tatuki0121 <148730025+tatuki0121@users.noreply.github.com>
2023-11-24 12:34:27 +0900

    ユーザー削除

--- remondou/admin-user-delete-confir.php
+++ remondou/admin-user-delete-confir.php
@@ -40,6 +40,8 @@ if(isset($_POST['return'])){
         ]; 
     }
     echo '</table>';
+
+    echo '上記の情報を削除しますか？';
     
     echo '<td><form action="admin-user-delete-confir.php" method="post">';
     echo '<input type="hidden" name="user_id" value="',$user_id,'">';
