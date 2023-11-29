7be3eaedfc89b545e38e45d43c626b4a79e0c71f
harutosaikyo <2201396@s.asojuku.ac.jp>
2023-11-29 14:27:32 +0900

    Update logout-input-admin.php

--- remondou/logout-input-admin.php
+++ remondou/logout-input-admin.php
@@ -1,10 +1,22 @@
 <?php session_start(); ?>
-<p><img src="image/!.jpg"></p>
+
+
+<p><img src="image/!.png" class="centered-image"></p>
+
+<div class = "all">
+
+<div class = "logout">
 <p>ログアウトしますか？</p>
+</div>
+<link rel = "stylesheet" href = "css/logout-input-admin.css">
+
+<div class = "form">
 <form action="admin-top.php" method="POST">
     <input type="submit" value="トップ画面へ">
 </form>
 <form action="logout-output-admin.php" method="POST">
     <input type="submit" value="ログアウト">
 </form>
+</div>
+</div>
 <?php require 'footer.php'; ?>
