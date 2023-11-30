907caabcd58821002c40578706a080c8ae326c1e
harutosaikyo <2201396@s.asojuku.ac.jp>
2023-11-30 09:59:33 +0900

    Update admin-shohinkousin.php

--- remondou/admin-shohinkousin.php
+++ remondou/admin-shohinkousin.php
@@ -20,7 +20,7 @@
     ?>
     <h2>商品更新</h2>
     <hr>
-
+<div class = "center">
     <form action="admin-update-output.php" method="post">
         <input type="hidden" name="shohin_id" value="<?= $shohin_id ?>">
         <p>商品名：<input type="text" name="name" required></p>
@@ -30,9 +30,10 @@
         <p>在庫数量：<input type="number" name="suryo" required></p>
         <p>商品画像：<input type="file" name="img" required></p>
         <p>商品説明：<input type="text" name="exp" required></p>
-        <input type="button" onclick="location.href='adimin-shohinitiran.php'" value="戻る">
+        <input type="button" onclick="location.href='admin-shohinitiran.php'" value="戻る">
         <input type="submit" value="更新">
     </form>
+</div>
 </body>
 
 </html>
