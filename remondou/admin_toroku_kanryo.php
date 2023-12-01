d72047f544d98e0774404219c0e3083f6889d3bc
kashio2201406 <148730004+kashio2201406@users.noreply.github.com>
2023-12-01 10:49:19 +0900

    セッションの二重登録修正　商品管理登録

--- remondou/admin_toroku_kanryo.php
+++ remondou/admin_toroku_kanryo.php
@@ -1,8 +1,9 @@
 <?php session_start(); ?>
 <?php require 'db-connect.php'; ?>
 <?php require 'admin-header.php';
-
-
+if (!isset($_SESSION['name'], $_SESSION['capa'], $_SESSION['dosu'], $_SESSION['price'], $_SESSION['suryo'], $_SESSION['img'], $_SESSION['exp'])) {
+    die("初めからやり直してください。<a href='https://aso2201406.penne.jp/remondou/admin_toroku_input.php'>初めからやり直す</a>");
+}
 // セッション変数からデータを取得
 $name = $_SESSION['name'];
 $capa = $_SESSION['capa'];
@@ -12,11 +13,15 @@ $suryo = $_SESSION['suryo'];
 $img = $_SESSION['img'];
 $exp = $_SESSION['exp'];
 
+// データベースに接続
 $pdo = new PDO($connect, USER, PASS);
 
 // データベースに商品を登録するクエリ
 $stmt = $pdo->prepare('INSERT INTO shohin (name, exp, volume, alcohol, price, stock, image) VALUES (?, ?, ?, ?, ?, ?, ?)');
 $stmt->execute([$name, $exp, $capa, $dosu, $price, $suryo, $img]);
+
+// 商品登録の処理が終わったらセッションを終了
+session_destroy();
 ?>
 
 <h1>商品登録</h1>
