c7b9d7665468afcc4a2c0254fc51689173818578
kashio2201406 <148730004+kashio2201406@users.noreply.github.com>
2023-12-01 12:01:15 +0900

    セッションの二重登録修正　商品管理登録

--- remondou/admin_toroku_kanryo.php
+++ remondou/admin_toroku_kanryo.php
@@ -1,17 +1,23 @@
 <?php session_start(); ?>
 <?php require 'db-connect.php'; ?>
-<?php require 'admin-header.php';
-if (!isset($_SESSION['name'], $_SESSION['capa'], $_SESSION['dosu'], $_SESSION['price'], $_SESSION['suryo'], $_SESSION['img'], $_SESSION['exp'])) {
+<?php
+$css = 'admin_toroku_kanryo.css';
+require 'admin-header.php';
+if (
+    !isset($_SESSION['item']['name']) && !isset($_SESSION['item']['capa']) && !isset($_SESSION['item']['dosu']) &&
+    !isset($_SESSION['item']['price']) && !isset($_SESSION['item']['suryo']) && !isset($_SESSION['item']['img']) &&
+    !isset($_SESSION['item']['exp'])
+) {
     die("初めからやり直してください。<a href='https://aso2201406.penne.jp/remondou/admin_toroku_input.php'>初めからやり直す</a>");
 }
 // セッション変数からデータを取得
-$name = $_SESSION['name'];
-$capa = $_SESSION['capa'];
-$dosu = $_SESSION['dosu'];
-$price = $_SESSION['price'];
-$suryo = $_SESSION['suryo'];
-$img = $_SESSION['img'];
-$exp = $_SESSION['exp'];
+$name = $_SESSION['item']['name'];
+$capa = $_SESSION['item']['capa'];
+$dosu = $_SESSION['item']['dosu'];
+$price = $_SESSION['item']['price'];
+$suryo = $_SESSION['item']['suryo'];
+$img = $_SESSION['item']['img'];
+$exp = $_SESSION['item']['exp'];
 
 // データベースに接続
 $pdo = new PDO($connect, USER, PASS);
@@ -21,7 +27,7 @@ $stmt = $pdo->prepare('INSERT INTO shohin (name, exp, volume, alcohol, price, st
 $stmt->execute([$name, $exp, $capa, $dosu, $price, $suryo, $img]);
 
 // 商品登録の処理が終わったらセッションを終了
-session_destroy();
+unset($_SESSION['item']);
 ?>
 
 <h1>商品登録</h1>
@@ -30,11 +36,11 @@ session_destroy();
 <?php
 if ($stmt->rowCount() > 0) {
     // 登録が成功した場合のメッセージを表示
-    echo "商品登録が完了しました";
+    echo "<p>商品登録が完了しました</p>";
 } else {
-    echo "商品登録に失敗しました。";
+    echo "<p>商品登録に失敗しました。</p>";
 }
 ?>
 <form action="admin-shohinitiran.php" method="post">
-    <input type="submit" name="admin-shohinitiran.php" value="商品一覧に戻る">
+    <p><input type="submit" name="admin-shohinitiran.php" value="商品一覧に戻る"></p>
 </form>
