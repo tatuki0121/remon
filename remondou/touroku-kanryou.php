3b164d6b1c0cd77f5df13c5d7ff5e6d584e0f77d
keishu0907 <2201395@s.asojuku.ac.jp>
2023-12-01 13:06:16 +0900

    上滝のcssです

--- remondou/touroku-kanryou.php
+++ remondou/touroku-kanryou.php
@@ -1,6 +1,9 @@
+<?php $css= 'touroku-kanryou.css'; ?>
 <?php require 'header.php'; ?>
 <form action="login-1.php" method="POST">
-    <p>登録が完了しました</p>
+    <h2>登録が完了しました</h2>
+    <div id="button">
     <input type="submit" value="ログイン画面へ">
+    </div>
 </form>
 <?php require 'footer.php'; ?>
