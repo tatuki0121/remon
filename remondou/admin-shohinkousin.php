714c9fa8ebc9cbf2938bbaca2100f95b252dbefe
kashio2201406 <148730004+kashio2201406@users.noreply.github.com>
2023-12-04 17:43:24 +0900

    admin-shohinkousin.css、admin-output.css　作り始め

--- remondou/admin-shohinkousin.php
+++ remondou/admin-shohinkousin.php
@@ -1,18 +1,19 @@
 <?php session_start(); ?>
 <?php require 'db-connect.php'; ?>
+<?php $css = 'admin-shohinitiran.css'; ?>
 <?php require 'admin-header.php'; ?>
 
-    <?php
-    $shohin_id = $_POST['shohin_id'];
-    //
-    $pdo=new PDO($connect, USER, PASS);
-    $sql = $pdo->prepare('select * from shohin where shohin_id = ?');
-    $sql->execute([$shohin_id]);
-    $row = $sql->fetch();
-    ?>
-    <h2>商品更新</h2>
-    <hr>
-<div class = "center">
+<?php
+$shohin_id = $_POST['shohin_id'];
+//
+$pdo = new PDO($connect, USER, PASS);
+$sql = $pdo->prepare('select * from shohin where shohin_id = ?');
+$sql->execute([$shohin_id]);
+$row = $sql->fetch();
+?>
+<h2>商品更新</h2>
+<hr>
+<div class="center">
     <form action="admin-update-output.php" method="post">
         <input type="hidden" name="shohin_id" value="<?= $shohin_id ?>">
         <p>商品名：<input type="text" name="name" value="<?= $row['name'] ?>" required></p>
