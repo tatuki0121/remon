a9746562cbec50df59fa0b115bafc9a794e29786
harutosaikyo <2201396@s.asojuku.ac.jp>
2023-12-04 16:01:13 +0900

    Update admin-update-kanryo.php

--- remondou/admin-update-kanryo.php
+++ remondou/admin-update-kanryo.php
@@ -3,15 +3,15 @@ session_start();
 require 'db-connect.php';
 require 'admin-header.php';
 
-$name = $_SESSION['name'];
-$capa = $_SESSION['capa'];
-$dosu = $_SESSION['dosu'];
-$price = $_SESSION['price'];
-$suryo = $_SESSION['suryo'];
-$img = $_SESSION['img'];
-$exp = $_SESSION['exp'];
+$name = $_SESSION['shohin']['name'];
+$capa = $_SESSION['shohin']['capa'];
+$dosu = $_SESSION['shohin']['dosu'];
+$price = $_SESSION['shohin']['price'];
+$suryo = $_SESSION['shohin']['suryo'];
+$img = $_SESSION['shohin']['img'];
+$exp = $_SESSION['shohin']['exp'];
 
-$shohin_id = $_SESSION['id'];
+$shohin_id = $_SESSION['shohin']['id'];
 
 $pdo = new PDO($connect, USER, PASS);
 
@@ -30,6 +30,7 @@ if ($stmt->rowCount() > 0) {
 } else {
     echo "商品更新に失敗しました。";
 }
+unset($_SESSION['shohin']);
 ?>
 <form action="admin-shohinitiran.php" method="post">
     <input type="submit" name="admin-shohinitiran.php" value="商品一覧に戻る">
