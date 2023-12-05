d0ed925b465649535e097a986639c42af23bd684
harutosaikyo <2201396@s.asojuku.ac.jp>
2023-12-05 10:28:52 +0900

    Update logout-output-admin.php

--- remondou/logout-output-admin.php
+++ remondou/logout-output-admin.php
@@ -1,12 +1,30 @@
-<?php session_start();?>
-<?php
-if(isset($_SESSION['admin'])){
-    unset($_SESSION['admin']);
-    echo 'ログアウト完了しました。';
-}else{
-    echo'すでにログアウトしています。';
-}
-?>
-<form action="admin-login.php">
-    <input type="submit" value="ログイン画面へ">
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/logout-output-admin.css">
+    <title>ログアウト</title>
+</head>
+<body>
+
+<div class="container">
+    <?php
+    if (isset($_SESSION['admin'])) {
+        unset($_SESSION['admin']);
+        echo '<div class="logout-message">ログアウト完了しました。</div><br><br>';
+    } else {
+        echo '<div class="logout-message">すでにログアウトしています。</div><br><br>';
+    }
+    ?>
+
+    <form action="admin-login.php">
+        <input type="submit" value="ログイン画面へ">
+    </form>
+</div>
+
 <?php require 'footer.php'; ?>
+
+</body>
+</html>
