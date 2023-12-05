836efa68f215c14cd28c0055fa596b344f21ed6e
harutosaikyo <2201396@s.asojuku.ac.jp>
2023-12-05 10:28:58 +0900

    Update logout-input-admin.php

--- remondou/logout-input-admin.php
+++ remondou/logout-input-admin.php
@@ -1,22 +1,33 @@
 <?php session_start(); ?>
 
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/logout-input-admin.css">
+    <title>ログアウト</title>
+</head>
+<body>
 
-<p><img src="image/!.png" class="centered-image"></p>
+<div class="all">
+    <p><img src="image/!.png" class="centered-image"></p>
 
-<div class = "all">
+    <div class="logout">
+        <p>ログアウトしますか？</p>
+    </div>
 
-<div class = "logout">
-<p>ログアウトしますか？</p>
+    <div class="form">
+        <form action="admin-top.php" method="POST">
+            <input type="submit" value="トップ画面へ">
+        </form>
+        <form action="logout-output-admin.php" method="POST">
+            <input type="submit" value="ログアウト">
+        </form>
+    </div>
 </div>
-<link rel = "stylesheet" href = "css/logout-input-admin.css">
 
-<div class = "form">
-<form action="admin-top.php" method="POST">
-    <input type="submit" value="トップ画面へ">
-</form>
-<form action="logout-output-admin.php" method="POST">
-    <input type="submit" value="ログアウト">
-</form>
-</div>
-</div>
 <?php require 'footer.php'; ?>
+
+</body>
+</html>
