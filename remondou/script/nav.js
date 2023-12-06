528b51fdbe324d8da09236f143b009dc5a3fc5be
keishu0907 <2201395@s.asojuku.ac.jp>
2023-12-06 10:52:32 +0900

    navデザイン入れたよ

--- remondou/script/nav.js
+++ remondou/script/nav.js
@@ -1,25 +1,33 @@
 new Vue({
     el: '#app',
-    data(){
-        return{
-            menus: [{
-                label: '檸檬堂'
-            },{
-                label: 'トップ',
-                path: './top.php'
-            },{
-                label: '商品一覧',
-                path: './shohinitirankensaku.php'
-            },{
-                label: 'カート',
-                path: './cart.php'
-            },{
-                label: 'ユーザー情報更新',
-                path: './user-update.php'
-            },{
-                label: 'ログアウト',
-                path: './logout-input-user.php'
-            }]
+    data() {
+        return {
+            menus: [
+                {
+                    label: '檸檬堂',
+                    highlighted: 'highlighted'
+                },
+                {
+                    label: 'トップ',
+                    path: './top.php'
+                },
+                {
+                    label: '商品一覧',
+                    path: './shohinitirankensaku.php'
+                },
+                {
+                    label: 'カート',
+                    path: './cart.php'
+                },
+                {
+                    label: 'ユーザー情報更新',
+                    path: './user-update.php'
+                },
+                {
+                    label: 'ログアウト',
+                    path: './logout-input-user.php'
+                }
+            ]
         };
     }
 });
