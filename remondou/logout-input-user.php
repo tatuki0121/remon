6d80a05ea5c6dedd4a62e4aee494473709ac4aac
2201401 <0113satoshi3104@gmail.com>
2023-12-06 09:45:25 +0900

    修正

--- remondou/logout-input-user.php
+++ remondou/logout-input-user.php
@@ -1,8 +1,8 @@
 <?php session_start(); ?>
 <?php require 'header.php'; ?>
+<?php $css = 'logout-input-user.css'; ?>
 <?php require 'nav.php'; ?>
 <p>ログアウトしますか？</p>
-<link rel="stylesheet" href="css/logout-input-user.css">
 <form action="logout-output-user.php">
     <p><input type="submit" value="ログアウト"></p>
 <?php require 'footer.php'; ?>
