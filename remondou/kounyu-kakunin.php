4308d243c507ffc8e061662b7c49a662e9605772
keishu0907 <2201395@s.asojuku.ac.jp>
2023-12-08 15:33:49 +0900

    購入確認

--- remondou/kounyu-kakunin.php
+++ remondou/kounyu-kakunin.php
@@ -1,20 +1,23 @@
 <?php require 'db-connect.php'; ?>
+<?php $css = 'kounyu-kakunin.css'; ?>
 <?php require 'nav.php'; ?>
 <?php
     $pdo=new PDO($connect, USER, PASS);
     $sql=$pdo->prepare('select * from shohin where shohin_id=?');
     $sql->execute([$id]);
     foreach($sql as $row){
-        echo '<div class="base">';
+        echo '<div id="shohin">';
         echo '<p>', '<img src="image/', $row['image'], '"width="100" height="100"></p>';
         echo '<form action="kounyu-kanryou.php" method="post">';
         echo '<p>商品名：', $row['name'], '</p>';
         echo '<p>容量：', $row['volume'], '<p>';
         echo '<p>価格：', $row['price'], '</p>';
         echo '<p>数量：', $_POST['stock'], '</p>';
+        echo '</div>';
         echo '<input type="hidden" name="shohin_id" value="', $row['shohin_id'], '">';
         echo '<input type="hidden" name="stock" value="', $_POST['stock'], '">';
-        echo '<p><input type="submit" name="nowbuy2" value="購入する"></p>';
+        echo '<div id="button">';
+        echo '<input type="submit" name="nowbuy2" value="購入する">';
         echo '</form>';
         echo '<form action="shohinitirankensaku.php" method="post">';
         echo '<input type="submit" value="商品一覧検索画面に戻る">';
