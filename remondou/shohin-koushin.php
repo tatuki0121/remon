ec86e9eabf3bab3223ea47f070c3e15c9916b60f
hakutojp <2201365@s.asojuku.ac.jp>
2023-12-08 15:54:35 +0900

    不要ファイル削除

--- remondou/shohin-koushin.php
+++
@@ -1,7 +0,0 @@
-<?php require 'header.php'; ?>
-<?php require 'menu.php'; ?>
-<form action="login-1.php" method="POST">
-    <p>更新完了しました</p>
-    <input type="submit" value="商品一覧画面へ">
-</form>
-<?php require 'footer.php'; ?>
