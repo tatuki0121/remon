4bdbe880f75c9590117c9bb9ec86f4794d76d7c0
keishu0907 <2201395@s.asojuku.ac.jp>
2023-12-12 13:01:03 +0900

    カート一覧完成！

--- remondou/cart.php
+++ remondou/cart.php
@@ -6,25 +6,48 @@
 $pdo=new PDO($connect, USER, PASS);
 if(!empty($_SESSION['item'])){
     if(!empty($_SESSION['item'])){
+        $cnt = 0;
         $sql=$pdo->prepare('select * from shohin where shohin_id=?');
+        
         foreach($_SESSION['item'] as $id=>$item){
             $sql->execute([$id]);
             $row = $sql->fetch();
-            echo '<form action="cart.php" method="post">';
-            echo '<div id="cart">';
-            echo '<p><img src="image/', $row['image'], '" width="100" height="100"></p>';
-            echo '<p>', $row['name'], '</p>';
-            echo '<p>個数：', $item['stock'], '</p>';
-            echo '<p><a href="cart-delete.php?id=', $id, '">削除</a></p>';
-            echo '</div>';
-            echo '</form>';
+            if( $cnt % 4 == 0 ){
+                echo '<div class="columns is-desktop">';
+            }
+            
+            //echo '<form action="cart.php" method="post">';
+            echo '<div class="column">
+                <div class="card">
+                    <header class="card-header">
+                        <p class="card-header-title">',$row['name'],'</p>
+                    </header>
+                    <div class="card-image">
+                        <figure class="image">
+                        <img src="image/', $row['image'], '">
+                        </figure>
+                    </div>
+                    <div class="card-content">
+                        <div class="content"><p>', $row['name'], '</p><p>個数：', $item['stock'], '</p>
+                            <br>
+                            ', $row['exp'],'<br><br><p><a href="cart-delete.php?id=', $id, '">削除</a></p>
+                        </div>
+                    </div>
+                </div>
+            </div>';
+            //echo '</form>';
+            $cnt++;
+            if( $cnt % 4 == 0 ){
+                echo '</div>';
+            }
+           
         }
+        echo '</div>';
         echo '<form action ="kounyu-kanryou.php" method ="post">';
         echo '<div id="button">';
         echo '<input type = "submit"  name="cartbuy" value = "購入する">';
         echo '</div>';
         echo '</form>';
-        
     }
 }else{
     echo '<div id="cartno">';
